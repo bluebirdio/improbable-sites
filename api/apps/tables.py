03e8a7998ae28a3362e87b33518f0c4c4924bd40
Allie Micka <allie@bluebird.io>
2020-03-28 15:53:17 -0500

    Add stack_id to App table.

--- api/apps/tables.py
+++ api/apps/tables.py
@@ -4,6 +4,7 @@ from sqlalchemy import ForeignKey
 
 class App(TextIdentified, ImprobableDbModel, Description):
     team_id = Column(ForeignKey('team._id', ondelete='CASCADE'), nullable=False)
+    stack_id = Column(ForeignKey('stack._id', ondelete='RESTRICT'), nullable=False)
     production_url = Column(String(255))
     repository_url = Column(String(255))
 
