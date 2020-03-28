ac7bcf268da67597e28bb63b9d2375f033d5fa0f
Allie Micka <allie@bluebird.io>
2020-03-28 12:50:10 -0500

    Add instances/apps_instances to path.

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from api.settings import *
 from api.users.api import router as users
 from api.roles.api import router as roles
 from api.teams.api import router as teams
+from api.apps.api import router as apps
 from api.environments.api import router as environments
 from api.repositories.api import router as repositories
-from api.apps.api import router as apps
+from api.instances.api import router as instances
+from api.apps_instances.api import router as apps_instances
 import uvicorn
 
 
@@ -20,6 +22,8 @@ api.add_middleware(DBSessionMiddleware, db_url=DATABASE_URL)
 # Allow each module to define its own routes separately and include them here with namespaces.
 router = APIRouter()
 router.include_router(apps, prefix="/apps", tags=["Apps"])
+router.include_router(apps_instances, prefix="/apps", tags=["Apps"])
+router.include_router(instances, prefix="/instances", tags=["Apps"])
 router.include_router(environments, prefix="/environments", tags=["Apps"])
 router.include_router(users, prefix="/users", tags=["Users and teams"])
 router.include_router(roles, prefix="/roles", tags=["Users and teams"])
