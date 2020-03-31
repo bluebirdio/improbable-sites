b83b8cdec98a2e5ce725d872822a9e7c13c77013
Allie Micka <allie@bluebird.io>
2020-03-31 14:57:04 -0500

    Travis configuration

---
+++ scripts/lint.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+black api tests --check
+isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --check-only api tests
