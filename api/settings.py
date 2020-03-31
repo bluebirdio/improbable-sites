7f6cfda779be89da68c48ef2b7152179e1218b80
Allie Micka <allie@bluebird.io>
2020-03-31 16:00:49 -0500

    Create a default config variable for DATABASE_URL

--- api/settings.py
+++ api/settings.py
@@ -1,5 +1,5 @@
 from os import environ
 
-DATABASE_URL = environ.get("DATABASE_URL")
+DATABASE_URL = environ.get("DATABASE_URL", "sqlite://")
 LISTEN_HOST = environ.get("LISTEN_HOST", "0.0.0.0")
 LISTEN_PORT = environ.get("LISTEN_PORT", 8000)
