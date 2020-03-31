930a75bafa56bd8a5e799b8eeb52732ad0af56f6
Allie Micka <allie@bluebird.io>
2020-03-31 16:09:07 -0500

    Include stacks tables in migrate.

--- api/migrate.py
+++ api/migrate.py
@@ -6,6 +6,7 @@ from api.instances.tables import *
 from api.repositories.tables import *
 from api.roles.tables import *
 from api.settings import DATABASE_URL
+from api.stacks.tables import *
 from api.teams.tables import *
 from api.users.tables import *
 
