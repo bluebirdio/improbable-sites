9c743ed038285cc0ab1864830e03e0546b835efd
Allie Micka <allie@bluebird.io>
2020-03-31 20:08:24 -0500

    Move tests to api/_tests and fix them up.

---
+++ api/_tests/test_teams.py
@@ -0,0 +1,41 @@
+from fastapi.testclient import TestClient
+
+from main import api
+
+client = TestClient(api)
+
+def path(action=None):
+    prefix = "/v1/teams/"
+    if action is not None:
+        return prefix + action
+    else:
+        return prefix
+
+
+def test_teams_get():
+    response = client.get(path())
+    assert response.status_code == 200
+
+
+def test_teams_crud():
+    # CREATE a team.
+    response = client.post(path(), json={"name": "Test Team",})
+    assert response.status_code == 200
+
+    content = response.json()
+    assert content["name"] == "Test Team"
+    team_id = content["id"]
+    assert team_id != ""
+
+    # TODO put
+
+    # GET the new team.
+    response = client.get(path(team_id))
+    assert response.status_code == 200
+
+    content = response.json()
+    assert content["name"] == "Test Team"
+
+    # DELETE the team.
+    response = client.delete(path(team_id))
+    assert response.status_code == 204
