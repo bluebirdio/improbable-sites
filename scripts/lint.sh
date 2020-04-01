10f79cf2f90a295b962adf5bb40b9d0cc4daee21
Allie Micka <allie@bluebird.io>
2020-03-31 20:15:30 -0500

    Remove test dir from lint.sh

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,5 +3,5 @@
 set -e
 set -x
 
-black api tests --check
-isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --check-only api tests
+black api --check
+isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --check-only api
