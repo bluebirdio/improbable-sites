e091e003742db277e22a6cbe99516b3425adba11
Allie Micka <allie@bluebird.io>
2020-03-31 23:00:47 -0500

    No 'sqlite3' driver in sqla

--- api/settings.py
+++ api/settings.py
@@ -3,7 +3,7 @@ from os import environ
 LISTEN_HOST = environ.get("LISTEN_HOST", "0.0.0.0")
 LISTEN_PORT = environ.get("LISTEN_PORT", 8000)
 
-DATABASE_URL = environ.get("DATABASE_URL", "sqlite3://")
+DATABASE_URL = environ.get("DATABASE_URL", "sqlite://")
 
 if DATABASE_URL[:6] == "sqlite":
     default_arguments = {"check_same_thread": False}
