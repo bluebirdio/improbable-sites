9300ed516cabd2493cebaeb1fb6b942ebd864421
Allie Micka <allie@bluebird.io>
2020-04-02 15:09:34 -0500

    Nullable url columns.

--- api/apps/tables.py
+++ api/apps/tables.py
@@ -6,5 +6,5 @@ from api.core.tables import *
 class App(TextIdentified, ImprobableDbModel, Description):
     team_id = Column(ForeignKey("team._id", ondelete="CASCADE"), nullable=False)
     stack_id = Column(ForeignKey("stack._id", ondelete="RESTRICT"), nullable=False)
-    production_url = Column(String(255))
-    repository_url = Column(String(255))
+    production_url = Column(String(255), nullable=True)
+    repository_url = Column(String(255), nullable=True)
