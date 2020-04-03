c6d41ab5a44e819ea67e2d1ba4c7c72050816846
Allie Micka <allie@bluebird.io>
2020-04-03 13:51:04 -0500

    set use_alter for parent_stack_id, add onupdate behavior.

--- api/stacks/tables.py
+++ api/stacks/tables.py
@@ -5,7 +5,7 @@ from api.core.tables import *
 
 class Stack(TextIdentified, ImprobableDbModel, Description):
     parent_stack_id = Column(
-        ForeignKey("stack._id", ondelete="RESTRICT"), nullable=True
+        ForeignKey("stack._id", onupdate="CASCADE", ondelete="RESTRICT", use_alter=True), nullable=True
     )
 
 
