27cd8277367db524ceb98112b0ae19a1442d92ac
Allie Micka <allie@bluebird.io>
2020-04-05 08:07:37 -0500

    Code cleanup

--- api/stacks/models.py
+++ api/stacks/models.py
@@ -5,6 +5,9 @@ from . import tables
 
 
 class Stack(HasDescription, ImprobableTextIdentified):
+    id: constr(
+        min_length=2, max_length=15, strip_whitespace=True, regex="[a-z0-9]"
+    ) = None
     parent_stack_id: str = None
 
     @validator("parent_stack_id")
