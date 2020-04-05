39aaa7191ea952852382e5a414567606c5e1f318
Allie Micka <allie@bluebird.io>
2020-04-04 21:45:36 -0500

    Add default environments

--- api/environments/values.py
+++ api/environments/values.py
@@ -5,3 +5,26 @@ class ProductionLevel(str, enum.Enum):
     PRODUCTION = "PRODUCTION"
     STAGING = "STAGING"
     DEVELOPMENT = "DEVELOPMENT"
+
+
+def default_environments():
+    return [
+        {
+            "id": "dev",
+            "name": "Development",
+            "production_level": "DEVELOPMENT",
+            "description": "Development environment",
+        },
+        {
+            "id": "stage",
+            "name": "Staging",
+            "production_level": "STAGING",
+            "description": "Staging environment",
+        },
+        {
+            "id": "prod",
+            "name": "Production",
+            "production_level": "PRODUCTION",
+            "description": "Production environment",
+        },
+    ]
