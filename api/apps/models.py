6d87546faa32533ba429d450631ea3611ad8dd15
Allie Micka <allie@bluebird.io>
2020-04-05 08:06:48 -0500

    Remove stack, repo info from apps.

--- api/apps/models.py
+++ api/apps/models.py
@@ -1,11 +1,11 @@
 from pydantic import AnyUrl
 
 from api.core.models import *
-from api.instances.models import Instance
 
 
 class App(HasDescription, ImprobableTextIdentified):
     team_id: str = ...
-    stack_id: str = ...
-    production_url: Optional[AnyUrl] = None
-    repository_url: Optional[AnyUrl] = None
+
+
+#    production_url: Optional[AnyUrl] = None
+#    repository_url: Optional[AnyUrl] = None
--- api/apps/tables.py
+++ api/apps/tables.py
@@ -7,8 +7,3 @@ class App(TextIdentified, ImprobableDbModel, Description):
     team_id = Column(
         ForeignKey("team._id", onupdate="CASCADE", ondelete="CASCADE"), nullable=False
     )
-    stack_id = Column(
-        ForeignKey("stack._id", onupdate="CASCADE", ondelete="RESTRICT"), nullable=False
-    )
-    production_url = Column(String(255), nullable=True)
-    repository_url = Column(String(255), nullable=True)
